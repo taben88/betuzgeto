f3e313d7994f8dc27502d6de7669e492cd5b7a03
taben88 <71879114+taben88@users.noreply.github.com>
2021-02-03 17:16:43 +0800

    Add files via upload

--- new.js
+++ new.js
@@ -1,85 +1,9 @@
 let lastN=[]
-
 let parent = document.getElementById('myApp')
-
-const alphabet = ["a", "á",
-    "b",
-    "c", "cs",
-    "d", "dz", "dzs",
-    "e", "é",
-    "f",
-    "g", "gy",
-    "h",
-    "i", "í",
-    "j",
-    "k",
-    "l", "ly",
-    "m",
-    "n", "ny",
-    "o", "ó", "ö", "ő",
-    "p",
-    "q",
-    "r",
-    "s", "sz",
-    "t", "ty",
-    "u", "ú", "ü", "ű",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z", "zs"
-];
-
-const words = [
-    { letter: 'a', word: 'autó', index: 0 },
-    { letter: 'á', word: 'sárkány', index: 1 },
-    { letter: 'b', word: 'baba', index: 0 },
-    { letter: 'c', word: 'maci', index: 2 },
-    { letter: 'cs', word: 'csörgő', index: 0 },
-    { letter: 'd', word: 'dínó', index: 0 },
-    { letter: 'dz', word: 'dz', index: 0 },
-    { letter: 'dzs', word: 'dzs', index: 0 },
-    { letter: 'e', word: 'gördeszka', index: 4 },
-    { letter: 'é', word: 'építőkocka', index: 0 },
-    { letter: 'f', word: 'fényképező', index: 0 },
-    { letter: 'g', word: 'gördeszka', index: 0 },
-    { letter: 'gy', word: 'gyík', index: 0 },
-    { letter: 'h', word: 'helikopter', index: 0 },
-    { letter: 'i', word: 'nyuszi', index: 5 },
-    { letter: 'í', word: 'síp', index: 1 },
-    { letter: 'j', word: 'jojó', index: 0 },
-    { letter: 'k', word: 'katona', index: 0 },
-    { letter: 'l', word: 'labda', index: 0 },
-    { letter: 'ly', word: 'üveggolyó', index: 2 },
-    { letter: 'm', word: 'maci', index: 0 },
-    { letter: 'n', word: 'fonal', index: 2 },
-    { letter: 'ny', word: 'nyuszi', index: 0 },
-    { letter: 'o', word: 'katona', index: 3 },
-    { letter: 'ó', word: 'szélforgó', index: 8 },
-    { letter: 'ö', word: 'gördeszka', index: 1 },
-    { letter: 'ő', word: 'repülő', index: 5 },
-    { letter: 'p', word: 'fényképező', index: 6 },
-    { letter: 'q', word: 'q', index: 0 },
-    { letter: 'r', word: 'robot', index: 0 },
-    { letter: 's', word: 'sárkány', index: 0 },
-    { letter: 'sz', word: 'számítógép', index: 0 },
-    { letter: 't', word: 'táska', index: 0 },
-    { letter: 'ty', word: 'tyúk', index: 0 },
-    { letter: 'u', word: 'ugrókötél', index: 0 },
-    { letter: 'ú', word: 'tyúk', index: 2 },
-    { letter: 'ü', word: 'üveggolyó', index: 0 },
-    { letter: 'ű', word: 'űrhajó', index: 0 },
-    { letter: 'v', word: 'vonat', index: 0 },
-    { letter: 'w', word: 'w', index: 0 },
-    { letter: 'x', word: 'xilofon', index: 0 },
-    { letter: 'y', word: 'y', index: 0 },
-    { letter: 'z', word: 'zebra', index: 0 },
-    { letter: 'zs', word: 'zsiráf', index: 0 }
-];
 
 class Field extends React.Component {
     render() {
-        return <span id="field" onTouchEnd={this.props.onTouchEnd} onDragOver={this.props.onDragOver} onDrop={this.props.onDrop}>{this.props.letter}</span>
+        return <span id="dropZone" onDragOver={this.props.onDragOver} onDrop={this.props.onDrop}>{this.props.letter}</span>
     }
 };
 
@@ -91,13 +15,13 @@ class Word extends React.Component{
         let child = document.getElementById(data);
         event.target.innerText=child.innerText;
         if (letter.toUpperCase() == event.target.innerText){
-            console.log("correct");
+            //console.log("correct");
             event.target.style.color = "#bfcb81";
             setTimeout(function(){
                 onCorrect();
                 event.target.style.color = "#40c5f2"}, 1000);
         } else {
-            console.log("incorrect");
+            //console.log("incorrect");
             event.target.style.color = "#e17b89";
         };
     };
@@ -111,7 +35,7 @@ class Word extends React.Component{
         let newWord = [];
         for (let i of word){
             if (i == "_"){
-                newWord.push(<Field letter={this.props.letter} onTouchEnd={this.handleTouchEnd} onDragOver={this.allowDrop} onDrop={(event)=>this.handleDrop(event, this.props.letter, this.props.onCorrect)}/>)
+                newWord.push(<Field letter={this.props.letter} onDragOver={this.allowDrop} onDrop={(event)=>this.handleDrop(event, this.props.letter, this.props.onCorrect)}/>)
             } else {
                 newWord.push(i)
             };
@@ -123,13 +47,41 @@ class Word extends React.Component{
 
 class Choice extends React.Component{
     render(){
-        return <span id={this.props.id} draggable={true} onDragStart={this.props.onDragStart}>{this.props.choice}</span>
+        return <span id={this.props.id} draggable={true} onDragStart={this.props.onDragStart} onTouchEnd= {this.props.onTouchEnd}>{this.props.choice}</span>
     };
 };
 
 class Choices extends React.Component{
     handleDrag(event) {
         event.dataTransfer.setData("text", event.target.id);
+    };
+
+    handleTouchEnd(event, letter, onCorrect){
+        event.preventDefault();
+        let container = document.getElementById("dropZone");
+        let containerXmin = container.getBoundingClientRect().x;
+        let containerXmax = container.getBoundingClientRect().right;
+        let containerYmin = container.getBoundingClientRect().y;
+        let containerYmax = container.getBoundingClientRect().bottom;
+        letter = letter || "";
+        let touch = event.changedTouches[0];
+        let touchX = touch.clientX;
+        let touchY = touch.clientY;
+        //console.log(touchX > containerXmin && touchX < containerXmax && touchY > containerYmin && touchY<containerYmax);
+        if (touchX > containerXmin && touchX < containerXmax && touchY > containerYmin && touchY < containerYmax){
+            //console.log(touchX, touchY);
+            container.innerHTML = event.target.innerHTML;
+            if (letter.toUpperCase() == container.innerText){
+                //console.log("correct");
+                container.style.color = "#bfcb81";
+                setTimeout(function(){
+                    onCorrect();
+                    container.style.color = "#40c5f2"}, 1000);
+            } else {
+                //console.log("incorrect");
+                container.style.color = "#e17b89";
+            };
+        };
     };
 
     render(){
@@ -158,11 +110,11 @@ class Choices extends React.Component{
         };
         //console.log(choices);
     return <div id="choices">
-            <Choice onDragStart={this.handleDrag} choice={choices[0]} id="choice1"/> &nbsp; 
-            <Choice onDragStart={this.handleDrag} choice={choices[1]} id="choice2"/> &nbsp;
-            <Choice onDragStart={this.handleDrag} choice={choices[2]} id="choice3"/> &nbsp;
-            <Choice onDragStart={this.handleDrag} choice={choices[3]} id="choice4"/> &nbsp;
-            <Choice onDragStart={this.handleDrag} choice={choices[4]} id="choice5"/> &nbsp;
+            <Choice onDragStart={this.handleDrag} onTouchEnd={(event)=>this.handleTouchEnd(event, this.props.letter, this.props.onCorrect)} choice={choices[0]} id="choice1"/> &nbsp; 
+            <Choice onDragStart={this.handleDrag} onTouchEnd={(event)=>this.handleTouchEnd(event, this.props.letter, this.props.onCorrect)} choice={choices[1]} id="choice2"/> &nbsp;
+            <Choice onDragStart={this.handleDrag} onTouchEnd={(event)=>this.handleTouchEnd(event, this.props.letter, this.props.onCorrect)} choice={choices[2]} id="choice3"/> &nbsp;
+            <Choice onDragStart={this.handleDrag} onTouchEnd={(event)=>this.handleTouchEnd(event, this.props.letter, this.props.onCorrect)} choice={choices[3]} id="choice4"/> &nbsp;
+            <Choice onDragStart={this.handleDrag} onTouchEnd={(event)=>this.handleTouchEnd(event, this.props.letter, this.props.onCorrect)} choice={choices[4]} id="choice5"/> &nbsp;
         </div>
     };
 };
@@ -200,7 +152,7 @@ class Quiz extends React.Component {
                 <img src={"assets/"+this.state.main.word + ".png" || null} alt={this.state.main.word}></img>
             </div>
             <div id="quiz" className="container vertical">
-                <Choices letter={this.state.main.letter} />
+                <Choices letter={this.state.main.letter} onCorrect={this.newQuiz}/>
                 <Word className="item" word={this.state.main.word} letter={this.state.main.letter} onCorrect={this.newQuiz}/>
             </div>
         </div>
